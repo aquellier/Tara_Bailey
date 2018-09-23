24d73ddf3e01123a55489f5e6eca9fe9814ad18f
antoinequellier <antoinecquellier@gmail.com>
2018-09-23 22:13:10 +0100

    adding illustrations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+console.log('Hello World from Webpacker');
 
 const womenswear = document.getElementById('womenswear');
 
@@ -20,3 +20,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
